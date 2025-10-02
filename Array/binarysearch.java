403c7036184fd3e9673e9a6a7770f9ed8bdea4c5
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-10-03 01:49:39 +0530

    new

--- Array/binarysearch.java
+++ Array/binarysearch.java
@@ -1,41 +1,32 @@
-public class binarysearch {
+public class binarysearch{
+    public static int binarys(int num[] , int key){
+        int start = 0;
+        int end = num.length-1;
+       
+        while(start<=end){
 
-    // Binary Search method
-    public static int binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
+            int mid = (start+end)/2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;  // prevent overflow
-
-            // Check if target is at mid
-            if (arr[mid] == target) {
-                return mid; // found, return index
+            if ( num[mid]==key){
+                return mid;
             }
-            // If target is greater, ignore left half
-            else if (arr[mid] < target) {
-                left = mid + 1;
-            }
-            // If target is smaller, ignore right half
-            else {
-                right = mid - 1;
-            }
-        }
 
-        // If element not found
-        return -1;
+            if ( num[mid]<key){
+                start = mid +1;
+            } 
+            if ( num[mid]>key){
+                end = mid -1;
+            }
+        } return -1;
+
+
     }
 
-    // Main method to test
-    public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10, 12, 14}; // must be sorted
-        int target = 10;
-
-        int result = binarySearch(numbers, target);
-
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found in array");
-        }
-    }
+    public static void main(String args[]){
+        int num[] = { 1,3,5,6,7,8,9};
+        int key = 6;
+        int result = binarys(num, key);
+     System.out.println(result);
+    
+}
 }
