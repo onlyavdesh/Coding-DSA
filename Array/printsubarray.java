c697657879afaa60341489e820dbbcaf93b90ec3
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-10-02 23:54:57 +0530

    new

---
+++ Array/printsubarray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class printsubarray {
+   
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array directly
+        System.out.println("Array: " + Arrays.toString(numbers));
+    }
+}
+
