66ec3812ba5d0e1a0483674861036ee6392a6731
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-10-05 21:22:27 +0530

    new

--- Array/Kardans.java
+++ Array/Kardans.java
@@ -3,18 +3,10 @@ public class Kardans {
         int currentsum = 0;
         int maxsum = Integer.MIN_VALUE;
         for ( int i = 0; i<arr.length;i++){
-            currentsum = currentsum +arr[i];
-            if ( currentsum < 0){
-                currentsum = 0;
-            }
-            if ( maxsum < currentsum ){
-              maxsum = currentsum;
-            } 
-            }
-            if (maxsum == 0){
-                System.out.println( " All values are ngative "); }
-        System.out.println("Your max subarray sum is "+maxsum);
-    }
+            currentsum = Math.max(arr[i],currentsum + arr[i]);
+             maxsum = Math.max(maxsum, currentsum) ;
+            } System.out.println("Your max sum is "+ maxsum);
+          }
     public static void main(String args[]){
         int arr[] = { -2,-3,-4,-6,-9,-5,-20};
         kardan(arr);
