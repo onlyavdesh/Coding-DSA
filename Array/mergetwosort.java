eb2dae4f633381914538e5a61c8d48a5c0c4ddd4
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-12 23:50:50 +0530

    change

---
+++ Array/mergetwosort.java
@@ -0,0 +1,11 @@
+public class mergetwosort {
+    public static void main (String args[]){
+        int a[] = { 1,2,3,5,6,7};
+        int b[] = { 5,6,8,9,90};
+        int n = a.length; 
+        int m = b.length; 
+
+        
+    }
+    
+}
