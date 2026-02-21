8113b6e67d38c8c34455ba717feae9734cfdd611
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-21 23:13:03 +0530

    fghj

---
+++ Sorting Algo /Palindrome111.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Palindrome111{
+
+    // Make method static
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("It is Palindrome ✅");
+        } else {
+            System.out.println("Not Palindrome ❌");
+        }
+
+        sc.close();
+    }
+}///////////
