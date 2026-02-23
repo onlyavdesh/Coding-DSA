ee637c621419fbe798ab3576eb7c815192dcea77
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-24 00:38:37 +0530

    String

---
+++ String/vowels.java
@@ -0,0 +1,15 @@
+package String;
+public class vowels {
+    public static void main(String args[]){
+        String s = "dfghjklvbnmqwertyuiopasdfghjkxcvbnmiiiiiiiiooooooooo";
+        int count = 0 ;
+        for (int i = 0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch =='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count ++;
+            }
+        }
+        System.out.print(count);
+    } 
+    
+}
