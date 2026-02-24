962dcae40b626ee9572269e72f066a4f036de64d
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-24 23:47:54 +0530

    change

---
+++ String/changestring.java
@@ -0,0 +1,17 @@
+package String;
+public class changestring {
+    // User function Template for Java
+    public static void main (String args[]){
+        String s = "sdfgDFGHJKFGHJKgh";
+        int n = s.length();
+        if (s.charAt(0) >= 65 && s.charAt(0) <= 90 ){
+            System.out.println(s.toUpperCase());
+        }
+        else {
+             System.out.println(s.toLowerCase());
+            
+        }
+    }
+}///// 
+    
+
