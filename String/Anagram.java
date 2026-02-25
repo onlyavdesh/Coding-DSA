e32424e94f7f1b18048a64f5b2cc5a2c0f330cf4
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-25 13:16:04 +0530

    Anagram

---
+++ String/Anagram.java
@@ -0,0 +1,11 @@
+package String;
+public class Anagram {
+    public static boolean Ana(String S){
+        
+       return true; 
+    }
+    public static void main(String args[]){
+
+    }
+    
+}
