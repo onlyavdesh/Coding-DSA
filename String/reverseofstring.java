42edc26928dc1ea06f592c2f5224d738b8a40d5b
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-02-26 23:08:01 +0530

    reverse of string

---
+++ String/reverseofstring.java
@@ -0,0 +1,28 @@
+package String;
+public class reverseofstring {
+    public static String reverseString(String s) {
+       int n = s.length();
+       if (n == 1){
+           return s;}
+       char[] arr = s.toCharArray();
+       int i = 0 ; 
+       int j = n-1;
+       while (i<=j){
+           char temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp;
+           i++;
+           j--;
+       }
+       String X = new String(arr);
+        
+        return X ;
+        
+    }
+    public static void main(String args[]){
+        String S = "fghjDFGHJK";
+        System.out.println(reverseString(S));
+    }
+}////////////
+    
+
