25ae51ab78cb8d163127d0976420d8e5140303ae
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-03-13 11:22:10 +0530

    2D array

---
+++ DimensionalArray/outputinArray.java
@@ -0,0 +1,14 @@
+package DimensionalArray;
+
+public class outputinArray {
+    public static void main(String args[]){
+        int [][] arr = new int[3][5];
+        for (int i = 0 ; i<arr.length;i++){
+            for (int j = 0 ; j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            } 
+            System.out.println( );
+        }
+    }
+    
+}
