3e06a31f6784efb703623097d1e479868526d056
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-03-13 12:02:43 +0530

    2d

---
+++ DimensionalArray/max_of_min.java
@@ -0,0 +1,19 @@
+package DimensionalArray;
+public class max_of_min {
+     public static void main(String[] args) {
+     int [][] arr = {{3,45,6,78,889,9},{6,5,434,3,322,67},{1,3,4,567,89,90}};
+     int max = Integer.MIN_VALUE;
+
+        for (int i = 0 ; i<arr.length;i++){ 
+                 int min = Integer.MAX_VALUE;
+            for (int j = 0 ; j<arr[0].length;j++){
+                if (arr[i][j]>max){
+                    max = arr[i][j];}
+                } 
+                if (min>max){
+                    min = max;
+                } 
+             } System.out.print(max);
+ } 
+    
+}
---
+++ DimensionalArray/maxsum.java
@@ -0,0 +1,21 @@
+package DimensionalArray;
+
+public class maxsum {
+ public static void main(String[] args) {
+     int [][] arr = {{3,45,6,78,889,9},{6,5,434,3,322,67},{1,3,4,567,89,90}};
+     int max = Integer.MIN_VALUE;
+     int row = 0 ; 
+        for (int i = 0 ; i<arr.length;i++){
+            int sum = 0 ; 
+            for (int j = 0 ; j<arr[0].length;j++){
+                sum += arr[i][j];
+            } 
+            max = Math.max(max,sum);
+            row = i ;
+         
+ }
+     System.out.println(max + " "+ row);
+
+ }
+}
+
