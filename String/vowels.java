c975c8517fbcc31fe97b245a9188f457e3cf548e
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2026-05-04 22:59:18 +0530

    changes

--- String/vowels.java
+++ String/vowels.java
@@ -1,7 +1,7 @@
 package String;
 public class vowels {
     public static void main(String args[]){
-        String s = "dfghjklvbnmqwertyuiopasdfghjkxcvbnmiiiiiiiiooooooooo";
+        String s = "dfghjklvbnmqwertyuiop";
         int count = 0 ;
         for (int i = 0 ; i<s.length(); i++){
             char ch = s.charAt(i);
